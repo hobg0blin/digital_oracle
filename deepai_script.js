6e5622ddad312353ef195e39b7be28d3d17ce74a
Brent Bailey <bb2906@nyu.edu>
2019-02-26 00:06:20 -0500

    wip

---
+++ deepai_script.js
@@ -0,0 +1,34 @@
+const env = require('node-env-file');
+const https = require('https');
+const fs = require('fs');
+
+env('.env');
+
+const deepai = require('deepai'); // OR include deepai.min.js as a script tag in your HTML
+
+deepai.setApiKey(process.env.DEEP_AI_KEY);
+
+https.get('https://rws-cards-api.herokuapp.com/api/v1/cards', (resp) => {
+  let data = '';
+
+  resp.on('data', chunk => {
+    console.log(chunk);
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    let result = JSON.parse(data);
+    console.log('result: ', result);
+    let titles = result.cards.map(i => {
+      return { 'name_short': i.name_short, 'desc': i.desc }
+    });
+
+  titles.map(title => {
+ response = deepai.callStandardApi("text2img", {
+        caption: `${title.desc}`,
+ }).then(result => {
+   console.log('result: ', result);
+ });
+});
+})
+})
--- index.js
+++ index.js
@@ -16,14 +16,37 @@ https.get('https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3', (resp) 
 
   resp.on('end', () => {
     let result = JSON.parse(data);
-    result.cards.map(card => {
-      console.log("card: ", card);
+    let sentimentTotal = 0;
+    //get all the cards
+    //TODO: maybe run this result through torch? or just generate a json file of 78 torch-created cards?
+    result.cards.map((card, index) => {
+      console.log(`Card ${index} is the ${card.name}, ${card.desc}, which means ${card.meaning_up}`);
       let analysis = sentiment.analyze(card.meaning_up);
-      console.log("analysis: ", analysis);
-      psList().then(result => {
-        console.log('ps list: ', result);
-      });
+      console.log(`The sentiment rating for this card is ${analysis.score}`);
+      sentimentTotal += analysis.score;
+    });
+    //get running processes, then act on result of the reading
+    let sentScore = sentimentTotal/3;
+    console.log('reading score: ', sentScore);
+    psList().then(result => {
+      console.log('result: ', result);
+      var rand = result[Math.floor(Math.random() * result.length)];
+      console.log('killing : ', rand);
+      if (sentScore > 3) {
+        //do something positive?
+      }
+      if (sentScore < 0) {
+     exec(`kill ${ rand.pid }`, (e, out, err) => {
+       if (e !== null) {
+         console.log('e: ', e);
+       } else {
+         console.log('stdout: ' + out);
+         console.log('stderr: ' + err);
+       }
+     })
 
+
+      }
      //      let running_scripts = require('child_process').exec('sh bash_script.sh', (e, out, err) => {
       //        console.log(`out: ${out}`);
       //        console.log(`err: ${err}`);
